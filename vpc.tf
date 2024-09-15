e2c52dfce8734a7c10cd39ccf7dbb96f9f27f683
abrarkhan <123900280+abkhan000@users.noreply.github.com>
2024-09-15 22:53:19 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,12 +6,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "dnamicro-eks-task-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 4
-  special = false
+  cluster_name = "dnamicro-eks-task"
 }
 
 module "vpc" {
