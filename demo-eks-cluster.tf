a15521758cf17766720890fcce927350cebc1de7
abrarkhan <123900280+abkhan000@users.noreply.github.com>
2024-09-16 01:52:52 +0530

    Update demo-eks-cluster.tf

--- demo-eks-cluster.tf
+++ demo-eks-cluster.tf
@@ -1,18 +1,20 @@
+
 provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc" "eks_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "eks-vpc"
+# Reuse an existing VPC instead of creating a new one
+data "aws_vpc" "existing_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["your-existing-vpc-name"]  # Replace with your actual VPC name
   }
 }
 
 resource "aws_subnet" "eks_subnet" {
   count = 2
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
+  vpc_id            = data.aws_vpc.existing_vpc.id
+  cidr_block        = cidrsubnet(data.aws_vpc.existing_vpc.cidr_block, 8, count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
     Name = "eks-subnet-${count.index}"
@@ -21,7 +23,7 @@ resource "aws_subnet" "eks_subnet" {
 
 resource "aws_eks_cluster" "karpenter_cluster" {
   name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
+  role_arn = data.aws_iam_role.eks_cluster_role.arn
   vpc_config {
     subnet_ids = aws_subnet.eks_subnet[*].id
   }
@@ -30,7 +32,7 @@ resource "aws_eks_cluster" "karpenter_cluster" {
 resource "aws_eks_node_group" "karpenter_node_group" {
   cluster_name    = aws_eks_cluster.karpenter_cluster.name
   node_group_name = "karpenter-ng"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
+  node_role_arn   = data.aws_iam_role.eks_node_role.arn
   subnet_ids      = aws_subnet.eks_subnet[*].id
   scaling_config {
     desired_size = 3
@@ -40,43 +42,28 @@ resource "aws_eks_node_group" "karpenter_node_group" {
   instance_types = ["t3.medium"]
 }
 
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = { Service = "eks.amazonaws.com" }
-    }]
-  })
+# Reuse existing IAM roles
+data "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"  # Reuse existing role
 }
 
-resource "aws_iam_role" "eks_node_role" {
-  name = "eks-node-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-    }]
-  })
+data "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"  # Reuse existing role
 }
 
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_node_role.name
+  role       = data.aws_iam_role.eks_node_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_node_role.name
+  role       = data.aws_iam_role.eks_node_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster_role.name
+  role       = data.aws_iam_role.eks_cluster_role.name
 }
 
 data "aws_availability_zones" "available" {}
